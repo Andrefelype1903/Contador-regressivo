8d8a5f51c7e4dffc4102e5f8732ed320c11623a5
andrefelype <andrefilype21122014@gmail.com>
2025-03-17 23:19:27 -0300

    add contador < 0

--- script/script.js
+++ script/script.js
@@ -1,42 +1,63 @@
+const contador = document.querySelector('.contador')
 
-const dataAlvo = new Date("may, 04, 2025 00:00:00").getTime();
+/* const dataAlvo = new Date("may, 04, 2025 00:00:00").getTime(); */
+
+const dataAlvo = new Date("mar, 17, 2025 23:16:35").getTime();
+
 
 const body = document.querySelector('body')
 
 console.log(dataAlvo);
+
+const canvas = () => {
+
+  const divHappy = document.createElement('div')
+  divHappy.classList.add("happy");
+  contador.appendChild(divHappy)
+
+  const newCanvas = document.createElement('canvas')
+  newCanvas.id = 'c';
+
+  divHappy.appendChild(newCanvas)
+}
 
 const contagem = () => {
 
   const dataAtual = new Date().getTime();
   const tempoRestante = dataAlvo - dataAtual;
 
-  if(tempoRestante < 0) {
+
+  if(tempoRestante <= 0) {
 
 
-    // body.style.backgroundImage = 'url(./img/andre.jpeg)'
+    canvas()
 
 
     clearInterval(intervalo);
 
-    return
+    location.reload()
+
+
+  } else {
+
+    const dias = Math.floor(tempoRestante / (1000 * 60  *  60  * 24)).toString().padStart(2, '0')
+  
+    const horas = Math.floor((tempoRestante % (1000 * 60  * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0')
+  
+    const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+  
+    const segundos = Math.floor((tempoRestante % (1000 * 60)) / 1000).toString().padStart(2, '0')
+  
+    console.log(dias + '/' + horas + '/' + minutos + '/' + segundos)
+    
+  
+    document.querySelector('#dias').innerHTML = dias
+    document.querySelector('#horas').innerHTML = horas
+    document.querySelector('#minutos').innerHTML = minutos
+    document.querySelector('#segundos').innerHTML = segundos
+    document.querySelector('#dias').innerHTML = dias
   }
 
-  const dias = Math.floor(tempoRestante / (1000 * 60  *  60  * 24)).toString().padStart(2, '0')
-
-  const horas = Math.floor((tempoRestante % (1000 * 60  * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0')
-
-  const minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
-
-  const segundos = Math.floor((tempoRestante % (1000 * 60)) / 1000).toString().padStart(2, '0')
-
-  console.log(dias + '/' + horas + '/' + minutos + '/' + segundos)
-  
-
-  document.querySelector('#dias').innerHTML = dias
-  document.querySelector('#horas').innerHTML = horas
-  document.querySelector('#minutos').innerHTML = minutos
-  document.querySelector('#segundos').innerHTML = segundos
-  document.querySelector('#dias').innerHTML = dias
 
 
 }
